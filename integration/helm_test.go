adba035cfaac06cd83d82be84a2e8fb8934f0dd4
Ross Fairbanks <rossf7@users.noreply.github.com>
2018-05-11 14:07:36 +0200

    Use apps/v1 for deployments (#10)
    * Bump version
    
    * Use v1/apps for deployments
    
    * Fix api version
    
    * Fix match labels
    
    * vendor: update e2e-harness
    
    * Add e2e tests for deployment manifests

--- integration/helm_test.go
+++ integration/helm_test.go
@@ -6,9 +6,17 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"reflect"
 	"testing"
 
 	"github.com/giantswarm/e2e-harness/pkg/framework"
+	"github.com/giantswarm/microerror"
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+const (
+	resourceNamespace = "kube-system"
 )
 
 var (
@@ -51,8 +59,59 @@ func TestHelm(t *testing.T) {
 		t.Errorf("unexpected error during installation of the chart: %v", err)
 	}
 
+	err = checkDeployment("nginx-ingress-controller", 3)
+	if err != nil {
+		t.Fatalf("controller manifest is incorrect: %v", err)
+	}
+
+	err = checkDeployment("default-http-backend", 2)
+	if err != nil {
+		t.Fatalf("default backend manifest is incorrect: %v", err)
+	}
+
 	err = framework.HelmCmd("test --debug test-deploy")
 	if err != nil {
 		t.Errorf("unexpected error during test of the chart: %v", err)
 	}
+}
+
+// checkDeployment ensures that key properties of the deployment are correct.
+func checkDeployment(name string, replicas int) error {
+	expectedMatchLabels := map[string]string{
+		"k8s-app": name,
+	}
+	expectedLabels := map[string]string{
+		"k8s-app":                    name,
+		"giantswarm.io/service-type": "managed",
+	}
+
+	c := f.K8sClient()
+	ds, err := c.Apps().Deployments(resourceNamespace).Get(name, metav1.GetOptions{})
+	if apierrors.IsNotFound(err) {
+		return microerror.Newf("could not find daemonset: '%s' %v", name, err)
+	} else if err != nil {
+		return microerror.Newf("unexpected error getting daemonset: %v", err)
+	}
+
+	// Check daemonset labels.
+	if !reflect.DeepEqual(expectedLabels, ds.ObjectMeta.Labels) {
+		return microerror.Newf("expected labels: %v got: %v", expectedLabels, ds.ObjectMeta.Labels)
+	}
+
+	// Check selector match labels.
+	if !reflect.DeepEqual(expectedMatchLabels, ds.Spec.Selector.MatchLabels) {
+		return microerror.Newf("expected match labels: %v got: %v", expectedMatchLabels, ds.Spec.Selector.MatchLabels)
+	}
+
+	// Check pod labels.
+	if !reflect.DeepEqual(expectedLabels, ds.Spec.Template.ObjectMeta.Labels) {
+		return microerror.Newf("expected pod labels: %v got: %v", expectedLabels, ds.Spec.Template.ObjectMeta.Labels)
+	}
+
+	// Check replica count.
+	if *ds.Spec.Replicas != int32(replicas) {
+		return microerror.Newf("expected replicas: %d got: %d", replicas, ds.Spec.Replicas)
+	}
+
+	return nil
 }
