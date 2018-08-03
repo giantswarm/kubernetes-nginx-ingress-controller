68a678bc7e64554d0b0c6c841834664a041b9f1d
Ross Fairbanks <ross@giantswarm.io>
2018-08-03 10:30:06 +0200

    Use separate values files for basic and migration tests

---
+++ integration/templates/ingress_controller_migration_values.go
@@ -0,0 +1,73 @@
+// +build k8srequired
+
+package templates
+
+// NginxIngressControllerMigrationValues values required by kubernetes-nginx-ingress-controller-chart.
+const NginxIngressControllerMigrationValues = `namespace: kube-system
+
+controller:
+  name: nginx-ingress-controller
+  k8sAppLabel: nginx-ingress-controller
+  metricsPort: 10254
+
+  replicas: 3
+
+  configmap:
+    name: ingress-nginx
+
+  image:
+    registry: quay.io
+    repository: giantswarm/nginx-ingress-controller
+    tag: 0.12.0
+
+  service:
+    enabled: false
+    nodePorts:
+      http: 30010
+      https: 30011
+
+  resources:
+    limits:
+      cpu: 500m
+      memory: 350Mi
+    requests:
+      cpu: 500m
+      memory: 350Mi
+
+defaultBackend:
+  name: default-http-backend
+  k8sAppLabel: default-http-backend
+  port: 8080
+
+  replicas: 2
+
+  image:
+    registry: quay.io
+    repository: giantswarm/defaultbackend
+    tag: 1.2
+
+  resources:
+    limits:
+      cpu: 10m
+      memory: 20Mi
+    requests:
+      cpu: 10m
+      memory: 20Mi
+
+global:
+  migration:
+    job:
+      enabled: true
+
+initContainer:
+  image:
+    registry: quay.io
+    repository: giantswarm/alpine
+    tag: 3.7
+
+test:
+  image:
+    registry: quay.io
+    repository: giantswarm/alpine-testing
+    tag: 0.1.0
+`
