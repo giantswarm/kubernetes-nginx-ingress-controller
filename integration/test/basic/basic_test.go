1071b7fe45bcc7f9ff9068cce23fbd6f7c6cd475
Ross Fairbanks <ross@giantswarm.io>
2018-08-27 16:37:07 +0200

    Updated test wiring

--- integration/test/basic/basic_test.go
+++ integration/test/basic/basic_test.go
@@ -4,62 +4,11 @@ package basic
 
 import (
 	"context"
-	"fmt"
 	"testing"
-
-	"github.com/giantswarm/e2etests/managedservices"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-
-	"github.com/giantswarm/kubernetes-nginx-ingress-controller/integration/env"
-	"github.com/giantswarm/kubernetes-nginx-ingress-controller/integration/templates"
-)
-
-const (
-	chartName          = "kubernetes-nginx-ingress-controller"
-	controllerName     = "nginx-ingress-controller"
-	defaultBackendName = "default-http-backend"
-	testName           = "basic"
 )
 
 func TestHelm(t *testing.T) {
-	chartConfig := managedservices.ChartConfig{
-		ChannelName: fmt.Sprintf("%s-%s", env.CircleSHA(), testName),
-		ChartName:   chartName,
-		ChartValues: templates.NginxIngressControllerBasicValues,
-		Namespace:   metav1.NamespaceSystem,
-	}
-	chartResources := managedservices.ChartResources{
-		Deployments: []managedservices.Deployment{
-			{
-				Name:      controllerName,
-				Namespace: metav1.NamespaceSystem,
-				Labels: map[string]string{
-					"app": controllerName,
-					"giantswarm.io/service-type": "managed",
-					"k8s-app":                    controllerName,
-				},
-				MatchLabels: map[string]string{
-					"k8s-app": controllerName,
-				},
-				Replicas: 3,
-			},
-			{
-				Name:      defaultBackendName,
-				Namespace: metav1.NamespaceSystem,
-				Labels: map[string]string{
-					"app": defaultBackendName,
-					"giantswarm.io/service-type": "managed",
-					"k8s-app":                    defaultBackendName,
-				},
-				MatchLabels: map[string]string{
-					"k8s-app": defaultBackendName,
-				},
-				Replicas: 2,
-			},
-		},
-	}
-
-	err := ms.Test(context.Background(), chartConfig, chartResources)
+	err := ms.Test(context.Background())
 	if err != nil {
 		t.Fatalf("%#v", err)
 	}
--- integration/test/basic/main_test.go
+++ integration/test/basic/main_test.go
@@ -3,6 +3,7 @@
 package basic
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/giantswarm/apprclient"
@@ -13,7 +14,16 @@ import (
 	"github.com/spf13/afero"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
+	"github.com/giantswarm/kubernetes-nginx-ingress-controller/integration/env"
 	"github.com/giantswarm/kubernetes-nginx-ingress-controller/integration/setup"
+	"github.com/giantswarm/kubernetes-nginx-ingress-controller/integration/templates"
+)
+
+const (
+	chartName          = "kubernetes-nginx-ingress-controller"
+	controllerName     = "nginx-ingress-controller"
+	defaultBackendName = "default-http-backend"
+	testName           = "basic"
 )
 
 var (
@@ -76,12 +86,47 @@ func init() {
 
 	{
 		c := managedservices.Config{
-			Namespace: metav1.NamespaceSystem,
-
 			ApprClient:    a,
 			HelmClient:    helmClient,
 			HostFramework: h,
 			Logger:        l,
+
+			ChartConfig: managedservices.ChartConfig{
+				ChannelName: fmt.Sprintf("%s-%s", env.CircleSHA(), testName),
+				ChartName:   chartName,
+				ChartValues: templates.NginxIngressControllerBasicValues,
+				Namespace:   metav1.NamespaceSystem,
+			},
+			ChartResources: managedservices.ChartResources{
+				Deployments: []managedservices.Deployment{
+					{
+						Name:      controllerName,
+						Namespace: metav1.NamespaceSystem,
+						Labels: map[string]string{
+							"app": controllerName,
+							"giantswarm.io/service-type": "managed",
+							"k8s-app":                    controllerName,
+						},
+						MatchLabels: map[string]string{
+							"k8s-app": controllerName,
+						},
+						Replicas: 3,
+					},
+					{
+						Name:      defaultBackendName,
+						Namespace: metav1.NamespaceSystem,
+						Labels: map[string]string{
+							"app": defaultBackendName,
+							"giantswarm.io/service-type": "managed",
+							"k8s-app":                    defaultBackendName,
+						},
+						MatchLabels: map[string]string{
+							"k8s-app": defaultBackendName,
+						},
+						Replicas: 2,
+					},
+				},
+			},
 		}
 		ms, err = managedservices.New(c)
 		if err != nil {
